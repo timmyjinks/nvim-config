e8d578a23dd68dc037c0df548e6b23012253406e
timmyjinks <tyson.j.jenkins.github@gmail.com>
2024-09-14 23:59:56 -0600

    Update keymaps and options

---
+++ lua/timmy/core/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+vim.cmd [[
+  syntax enable
+]]
+
+opt.number = true
+opt.relativenumber = true
+
+opt.cursorline = true
+
+opt.syntax = "on"
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = false
+
+opt.wildmode = "longest,list" 
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
