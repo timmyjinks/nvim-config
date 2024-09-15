dc758fa26abca116b4ac1e3127bce3e3c2f4e9cc
timmyjinks <tyson.j.jenkins.github@gmail.com>
2024-09-15 03:25:32 -0600

    Update plugin manager to use lsp

--- lua/timmy/lazy.lua
+++ lua/timmy/lazy.lua
@@ -11,4 +11,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("timmy.plugins")
+require("lazy").setup({{ import = "timmy.plugins" }, { import = "timmy.plugins.lsp" }}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
