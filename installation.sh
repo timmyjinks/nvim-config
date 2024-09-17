f207fdc20f924aada06557a24f21f025a90b1492
timmyjinks <tyson.j.jenkins.github@gmail.com>
2024-09-16 20:47:04 -0600

    Install based on the OS

--- installation.sh
+++ installation.sh
@@ -1,4 +1,15 @@
-sudo apt install ripgrep
-curl -L -O "https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim-linux64.deb"
-sudo apt install ./nvim-linux64.deb
-rm nvim-linux64.deb
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo apt install ripgrep
+  curl -L -O "https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim-linux64.deb"
+  sudo apt install ./nvim-linux64.deb
+  rm nvim-linux64.deb       # ...
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  choco install ripgrep neovim -y
+elif [[ "$OSTYPE" == "msys" ]]; then
+  choco install ripgrep neovim -y
+else
+  echo "OS not supported"
+fi
+
+
+
