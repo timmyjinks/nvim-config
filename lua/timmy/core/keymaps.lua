1d705e16bf36049e78ecfdf8bf695e021c8f94f4
timmyjinks <tyson.j.jenkins.github@gmail.com>
2024-09-22 22:01:21 -0600

    Add keymap for visual mode

--- lua/timmy/core/keymaps.lua
+++ lua/timmy/core/keymaps.lua
@@ -3,4 +3,5 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":noh<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>ns", "<S-v>", { desc = "Clear search highlights" })
 
