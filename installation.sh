4a8d00f5e72f70c6d36342e69d30a46bf7f759f8
timmyjinks <tysonjjenkins.github@gmail.com>
2026-06-06 20:25:29 -0600

    refactor: remove detect os and random new line

--- installation.sh
+++ installation.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-OS=""
-DISTRO=""
-
-detect_os() {
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    OS="macos"
-  elif [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    OS="$ID"
-  else
-    echo "Could not detect OS."
-    exit 1
-  fi
-}
-
 copy_config() {
   local config_dir="$HOME/.config/nvim"
   if [[ -d "$config_dir" ]]; then
-    echo ""
     read -rp ">>> $config_dir already exists. Overwrite? (y/N): " confirm
     if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
       echo ">>> Skipping config copy."
